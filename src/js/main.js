dedb92e6b6ed13aae69ef3996f28d170a92a62d1
Alexandr Petrovich <apalion97@gmail.com>
2024-12-12 14:30:24 +0200

    Add search component to multiple templates
    Introduced a new search block to `search-block.html` and integrated it into `Categories-Shop-Page.html` and `Product-card.html`. The search feature is styled consistently and includes a form with input and submission button, aiming to improve user navigation and usability.

--- src/js/main.js
+++ src/js/main.js
@@ -1,130 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('Loaded Scripts');
-
-  // const swiper = new Swiper('.mySwiper', {
-  //   slidesPerView: 4,
-  //   spaceBetween: 32,
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     dynamicBullets: true,
-  //   },
-  //   breakpoints: {
-  //     0: {
-  //       slidesPerView: 1,
-  //     },
-  //     769: {
-  //       slidesPerView: 2,
-  //     },
-  //     1025: {
-  //       slidesPerView: 3,
-  //     },
-  //     1346: {
-  //       slidesPerView: 4,
-  //     },
-  //   },
-  // });
-  //
-  // const swiper2 = new Swiper('.gallery-swiper', {
-  //   spaceBetween: 24,
-  //   slidesPerView: 3,
-  //   freeMode: true,
-  //   watchSlidesProgress: true,
-  //   loop: true,
-  //   breakpoints: {
-  //     1024: {
-  //       spaceBetween: 24,
-  //     },
-  //     100: {
-  //       spaceBetween: 16,
-  //     },
-  //   },
-  // });
-  //
-  // const swiper3 = new Swiper('.gallery-swiper-big', {
-  //   spaceBetween: 10,
-  //   zoom: true,
-  //   loop: true,
-  //   thumbs: {
-  //     swiper: swiper2,
-  //   },
-  // });
-  //
-  // const swiper4 = new Swiper('.swiper-blog', {
-  //   slidesPerView: 3,
-  //   spaceBetween: 32,
-  //   breakpoints: {
-  //     1024: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 32,
-  //     },
-  //     768: {
-  //       slidesPerView: 'auto',
-  //     },
-  //     640: {
-  //       slidesPerView: 'auto',
-  //     },
-  //     100: {
-  //       slidesPerView: 'auto',
-  //       spaceBetween: 20,
-  //     },
-  //   },
-  // });
-  //
-  // const swiper5 = new Swiper('.swiper-brands', {
-  //   slidesPerView: 8,
-  //   spaceBetween: 32,
-  //   loop: true,
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  //   breakpoints: {
-  //     1345: {
-  //       slidesPerView: 8,
-  //     },
-  //     1024: {
-  //       slidesPerView: 6,
-  //     },
-  //     768: {
-  //       slidesPerView: 'auto',
-  //     },
-  //     640: {
-  //       slidesPerView: 'auto',
-  //     },
-  //     100: {
-  //       slidesPerView: 'auto',
-  //     },
-  //   },
-  // });
-  //
-  // const swiper6 = new Swiper('.checkboxes-alphabet-list--swiper', {
-  //   slidesPerView: 'auto',
-  //   spaceBetween: 8,
-  // });
-  //
-  // const conditionSwiper = new Swiper('.condition-swiper', {
-  //   slidesPerView: 4,
-  //   spaceBetween: 32,
-  //   breakpoints: {
-  //     1024: {
-  //       slidesPerView: 4,
-  //     },
-  //     768: {
-  //       slidesPerView: 3,
-  //     },
-  //     640: {
-  //       slidesPerView: 2,
-  //     },
-  //     100: {
-  //       slidesPerView: 'auto',
-  //     },
-  //   },
-  // });
-
   const swiper1 = new Swiper('.swiper-container-categories', {
     slidesPerView: 4,
     spaceBetween: 24,
@@ -156,5 +30,75 @@ document.addEventListener('DOMContentLoaded', function() {
   burgerButton?.addEventListener('click', toggleMenu);
 });
 
+window.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('modal')
+  const openModalBtn = document.getElementById('openModal')
+  const closeModalElements = document.querySelectorAll('.close-modal')
 
-// TODO test
+  if (modal && openModalBtn && closeModalElements) {
+    const openModal = () => {
+      modal.classList.remove('hidden')
+    }
+
+    const closeModal = () => {
+      modal.classList.add('hidden')
+    }
+
+    openModalBtn.addEventListener('click', openModal)
+
+    closeModalElements.forEach(el => {
+      el.addEventListener('click', closeModal)
+    })
+
+    window.addEventListener('click', event => {
+      if (event.target === modal) {
+        closeModal()
+      }
+    })
+  } else {
+    // console.error('test')
+  }
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerButton = document.getElementById('burger-button')
+  const mobileMenu = document.getElementById('mobile-menu')
+
+  burgerButton?.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active')
+    document.body.classList.toggle('overflow-hidden')
+  })
+})
+
+document.querySelectorAll('.menu-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    const submenu = button.nextElementSibling
+
+    // Переключаем видимость подменю
+    button.classList.toggle('open-menu')
+    submenu.classList.toggle('hidden')
+
+    // Закрываем другие подменю, если они открыты
+    document.querySelectorAll('.submenu').forEach(menu => {
+      if (menu !== submenu) {
+        menu.classList.add('hidden')
+      }
+    })
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonSearch = document.querySelector('#search-button')
+  const searchContainer = document.querySelector('.searchContainer')
+
+  buttonSearch?.addEventListener('click', (e) => {
+    e.stopPropagation(); // Остановить всплытие события, чтобы не сработал обработчик на document
+    searchContainer?.classList.toggle('active');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (!searchContainer?.contains(e.target)) { // Если клик не внутри searchContainer
+      searchContainer?.classList.remove('active'); // Убрать класс active
+    }
+  });
+})
